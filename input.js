44b966cd5a2d01132caa0136b7ea109d7bd4158c
teksts <mark.egelhoff1@gmail.com>
2023-06-07 17:08:15 -0700

    Added keyboard commands to print canned messages

--- input.js
+++ input.js
@@ -16,6 +16,15 @@ const setupInput = (conn) => {
     if (key === 'd') {
       connection.write("Move: right");
     }
+    if (key === 'r') {
+      connection.write("Say: pwned!");
+    }
+    if (key === 'q') {
+      connection.write("Say: oops..");
+    }
+    if (key === 'x') {
+      connection.write("Say: i love snek");
+    }
     if (key === '\u0003') {
       process.exit();
     }
